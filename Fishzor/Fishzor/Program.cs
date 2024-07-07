7754e362e760fd34ff22431965612602c43a71fd
Don Kackman <dkackman@gmail.com>
2024-07-07 16:32:51 -0500

    azure

--- Fishzor/Fishzor/Program.cs
+++ Fishzor/Fishzor/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
