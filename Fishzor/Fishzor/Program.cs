592ec22529d1fc8e338f2eb355bb43ac1ee9e5e4
Don Kackman <dkackman@gmail.com>
2024-08-30 09:51:32 -0500

    server side logging

--- Fishzor/Fishzor/Program.cs
+++ Fishzor/Fishzor/Program.cs
@@ -5,8 +5,15 @@ using Fishzor.Client.State;
 using System.Net.Http;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Fishzor.Hubs;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -18,25 +25,35 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Application starting up");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    logger.LogInformation("Development environment detected");
 }
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    logger.LogInformation("Production environment detected");
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+logger.LogDebug("Configuring Razor Components");
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Fishzor.Client._Imports).Assembly);
+
+logger.LogDebug("Mapping SignalR hub");
 app.MapHub<FishHub>("/fishhub");
 
+logger.LogInformation("Application configured, starting to run");
 app.Run();
+logger.LogInformation("Application has stopped");
--- Fishzor/Fishzor/Services/FishService.cs
+++ Fishzor/Fishzor/Services/FishService.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.SignalR;
 using Fishzor.Hubs;
+using Microsoft.Extensions.Logging;
 
 namespace Fishzor.Services;
 
@@ -7,15 +8,19 @@ public class FishService
 {
     private int _fishCount = 0;
     private readonly IHubContext<FishHub> _hubContext;
+    private readonly ILogger<FishService> _logger;
 
-    public FishService(IHubContext<FishHub> hubContext)
+    public FishService(IHubContext<FishHub> hubContext, ILogger<FishService> logger)
     {
         _hubContext = hubContext;
+        _logger = logger;
+        _logger.LogInformation("FishService initialized");
     }
 
     public async Task<int> AddFish()
     {
         _fishCount++;
+        _logger.LogInformation("Fish added. New count: {FishCount}", _fishCount);
         await NotifyClients();
         return _fishCount;
     }
@@ -25,6 +30,11 @@ public class FishService
         if (_fishCount > 0)
         {
             _fishCount--;
+            _logger.LogInformation("Fish removed. New count: {FishCount}", _fishCount);
+        }
+        else
+        {
+            _logger.LogWarning("Attempted to remove fish when count is already 0");
         }
         await NotifyClients();
         return _fishCount;
@@ -32,11 +42,13 @@ public class FishService
 
     public int GetFishCount()
     {
+        _logger.LogDebug("Fish count requested: {FishCount}", _fishCount);
         return _fishCount;
     }
 
     private async Task NotifyClients()
     {
+        _logger.LogDebug("Notifying clients of updated fish count: {FishCount}", _fishCount);
         await _hubContext.Clients.All.SendAsync("ReceiveFishCount", _fishCount);
     }
 }
