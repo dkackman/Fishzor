e608362863f60ff1c4e661d56678896f185e7f22
Don Kackman <dkackman@gmail.com>
2024-09-15 10:26:34 -0500

    refactor velocity randomization

--- Fishzor/Fishzor.Client/Components/FishAnimation.cs
+++ Fishzor/Fishzor.Client/Components/FishAnimation.cs
@@ -4,16 +4,17 @@ public class FishAnimation
 {
     private static readonly Random _random = new();
     public Point Position { get; private set; } = new();
-    public Velocity Velocity { get; private set; } = GetRandomVelocity(_random.Next(0, 2) == 0 ? -1 : 1);
-    public Direction Direction => Velocity.dx > 0 ? Direction.Right : Direction.Left;
+    public Velocity Velocity { get; private set; } = GetRandomVelocity(_random.Next(0, 2) == 0 ? Direction.Left : Direction.Right);
     public bool Enabled { get; set; } = true;
 
+    // this is called when dragging the fish is complete
     public void MoveFish(Point newPosition)
     {
         Position = newPosition;
-        Velocity = GetRandomVelocity(Velocity.dx > 0 ? -1 : 1);
+        Velocity = GetRandomVelocity(Velocity.OtherDirection().Direction);
     }
 
+    // this is called on the animation loop
     public void IncrementPosition(Velocity velocity, ClientRect tank, ClientRect fish)
     {
         Velocity = velocity;
@@ -25,26 +26,30 @@ public class FishAnimation
         Position += Velocity;
     }
 
-    public static Velocity GetRandomVelocity(int direction) => new((_random.NextDouble() * 3.0 + 0.5) * direction, (_random.NextDouble() - 0.5) * 0.5);
+    public static Velocity GetRandomVelocity(Direction direction) 
+    {
+        var directionModifier = direction == Direction.Right ? 1 : -1;
+        return new((_random.NextDouble() * 3.0 + 0.5) * directionModifier, (_random.NextDouble() - 0.5) * 0.5);
+    }
 
     private void AdjustVelocityForBoundaries(Point nextPosition, ClientRect tankRect, ClientRect fish)
     {
         if (nextPosition.Top <= tankRect.Top)
         {
-            Velocity = new Velocity(Velocity.dx, Math.Abs(Velocity.dy));
+            Velocity = new Velocity(Velocity.Dx, Math.Abs(Velocity.Dy));
         }
         else if (nextPosition.Top + fish.Height >= tankRect.Bottom)
         {
-            Velocity = new Velocity(Velocity.dx, -Math.Abs(Velocity.dy));
+            Velocity = new Velocity(Velocity.Dx, -Math.Abs(Velocity.Dy));
         }
 
         if (nextPosition.Left <= tankRect.Left)
         {
-            Velocity = new Velocity(Math.Abs(Velocity.dx), Velocity.dy);
+            Velocity = new Velocity(Math.Abs(Velocity.Dx), Velocity.Dy);
         }
         else if (nextPosition.Left + fish.Width >= tankRect.Right)
         {
-            Velocity = new Velocity(-Math.Abs(Velocity.dx), Velocity.dy);
+            Velocity = new Velocity(-Math.Abs(Velocity.Dx), Velocity.Dy);
         }
     }
 }
--- Fishzor/Fishzor.Client/Components/Velocity.cs
+++ Fishzor/Fishzor.Client/Components/Velocity.cs
@@ -2,8 +2,10 @@ namespace Fishzor.Client.Components;
 
 public readonly struct Velocity(double dx, double dy)
 {
-    public double dx { get; } = dx;
-    public double dy { get; } = dy;
+    public double Dx { get; } = dx;
+    public double Dy { get; } = dy;
+    public Direction Direction => Dx > 0 ? Direction.Right : Direction.Left;
+    public Velocity OtherDirection() => new(-Dx, Dy);
 
-    public override readonly string ToString() => $"[dx: {dx}, dy: {dy}]";
+    public override readonly string ToString() => $"[dx: {Dx}, dy: {Dy}]";
 }
