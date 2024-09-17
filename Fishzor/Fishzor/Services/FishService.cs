db03701ccee83c5753f2bab7d967250a0d090019
Don Kackman <dkackman@gmail.com>
2024-09-17 11:04:38 -0500

    Random.Shared (who knew)

--- Fishzor/Fishzor/Services/FishService.cs
+++ Fishzor/Fishzor/Services/FishService.cs
@@ -6,8 +6,6 @@ namespace Fishzor.Services;
 
 public class FishService(ILogger<FishService> logger)
 {
-    private readonly Random _random = new();
-
     private readonly ILogger<FishService> _logger = logger;
     private readonly ConcurrentDictionary<string, FishState> _connectedFish = new();
     public IEnumerable<FishState> ConnectedFish => _connectedFish.Values;
@@ -18,14 +16,15 @@ public class FishService(ILogger<FishService> logger)
     {
         _logger.LogDebug("Client connected: {ConnectionId}", connectionId);
 
-        var scaleValue = _random.NextDouble() * (1.00 - 0.5) + 0.5;
+        var scaleValue = Random.Shared.NextDouble() * (1.00 - 0.5) + 0.5;
         var colors = Enum.GetValues(typeof(FishColor));
         var state = new FishState
         {
             Id = connectionId,
-            Color = (FishColor)(colors.GetValue(_random.Next(colors.Length)) ?? FishColor.Orange),
+            Color = (FishColor)(colors.GetValue(Random.Shared.Next(colors.Length)) ?? FishColor.Orange),
             Scale = scaleValue.ToString("0.##")
         };
+
         if (!_connectedFish.TryAdd(connectionId, state))
         {
             _logger.LogWarning("Client already exists {ConnectionId}", connectionId);
